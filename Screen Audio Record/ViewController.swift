3b62034e9cdf12fa4eafbf39b1f4316da44a7c4e
faisalashrafwani <faisalashrafwani@gmail.com>
2022-08-13 12:57:35 +0530

    Recording voice on screen tap added with time stamp

---
+++ Screen Audio Record/ViewController.swift
@@ -0,0 +1,125 @@
+//
+//  ViewController.swift
+//  Screen Audio Record
+//
+//  Created by admin on 13/08/22.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController, AVAudioRecorderDelegate {
+
+    @IBOutlet weak var recordView: UIView!
+    
+    var recordingSession: AVAudioSession!
+    var audioRecorder: AVAudioRecorder!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //SETTING UP RECORDING SESSION
+        recordingSession = AVAudioSession.sharedInstance()
+
+        do {
+            try recordingSession.setCategory(.playAndRecord, mode: .default)
+            try recordingSession.setActive(true)
+            recordingSession.requestRecordPermission() { [unowned self] allowed in
+                DispatchQueue.main.async {
+                    if allowed {
+                        
+                        //CREATING GESTURE RECOGNIZER
+                        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(self.gestureFired(_:)))
+                        gestureRecognizer.numberOfTapsRequired = 1
+                        gestureRecognizer.numberOfTouchesRequired = 1
+
+                        //SETTING GESTURE RECOGNIZER TO FILE
+                        self.recordView.addGestureRecognizer(gestureRecognizer)
+                        self.recordView.isUserInteractionEnabled = true
+                        
+                    } else {
+                        print("No Permission Granted")
+                    }
+                }
+            }
+        } catch {
+            print(error)
+        }
+    }
+    //HANDLES SCREEN GESTURES
+    @objc func gestureFired(_ gesture: UITapGestureRecognizer) {
+        if audioRecorder == nil {
+                startRecording()
+            } else {
+                finishRecording(success: true)
+            }
+    }
+    
+    //STARTS RECORDING
+    func startRecording() {
+        let captureDateTime = captureDateTime()
+        let audioFilename = getDocumentsDirectory().appendingPathComponent("recording \(captureDateTime).m4a")
+
+        let settings = [
+            AVFormatIDKey: Int(kAudioFormatMPEG4AAC),
+            AVSampleRateKey: 12000,
+            AVNumberOfChannelsKey: 1,
+            AVEncoderAudioQualityKey: AVAudioQuality.high.rawValue
+        ]
+
+        do {
+            audioRecorder = try AVAudioRecorder(url: audioFilename, settings: settings)
+            audioRecorder.delegate = self
+            audioRecorder.record()
+            self.recordView.backgroundColor = UIColor.green
+            print("Recording in progress...\n")
+            print("Start Time: \(captureDateTime)\n")
+        } catch {
+            finishRecording(success: false)
+        }
+    }
+    
+    //STOPS RECORDING
+    func finishRecording(success: Bool) {
+        let captureDateTime = captureDateTime()
+        audioRecorder.stop()
+        self.recordView.backgroundColor = UIColor.red
+        print("End Time: \(captureDateTime)\n")
+        audioRecorder = nil
+
+        if success {
+            print("Recording success\n")
+        } else {
+            print("recording failed.")
+        }
+    }
+    
+    //METHOD USED IF INTERRUPTIONS STOP RECORDING
+    func audioRecorderDidFinishRecording(_ recorder: AVAudioRecorder, successfully flag: Bool) {
+        if !flag {
+            finishRecording(success: false)
+        }
+    }
+    
+    //RETURNS PATH FOR SAVING FILE
+    func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        
+        //PRINTS PATH FOR SAVED FILES
+        print(paths)
+        return paths[0]
+    }
+    
+    //CAPTURES DATE AND TIME
+    func captureDateTime() -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH: mm: ssZ"
+        dateFormatter.timeZone = TimeZone(identifier: "UTC")
+        dateFormatter.locale = Locale(identifier: "en_US")
+        return (dateFormatter.string(from: Date()))
+    }
+
+
+}
+
